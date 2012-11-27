6953b32e28b9352690e3b25019bbdc13fce991f7
John Krauss <irving.krauss@gmail.com>
2012-11-27 01:39:36 -0500

    Allowing for interactive selection of vocab sections, feedback on which you're using, and generally spiffier styling

--- js/src/flashcard.js
+++ js/src/flashcard.js
@@ -11,49 +11,86 @@ $(document).ready(function () {
         $extra = $('#extra', $card),
         $next = $('#next', $card),
         $reveal = $('#reveal', $card),
-        vocabFolder = '../vocab',
-        vocabFiles = [
-            '1.2.txt',
-            '1.3.txt',
-            '1.4.txt',
-            '1.5.txt',
-            '1.Grundwortschatz.txt',
-            '1.exercise.txt'
-        ];
+        $sad = $('#sad'),
+        $sources = $('#sources'),
+        $indexLoader = $('<div />').css('display', 'none').appendTo($('body')),
+        vocabListPath = '../vocab/',
+        allVocab = {},
+        availableVocab = {},
 
-    $.when.apply(this, _.map(vocabFiles, function (fileName) {
-        return $.get(vocabFolder + '/' + fileName);
-    })).done(function () {
-        var vocab = [],
-            contents = _.pluck(arguments, 0);
-
-        // Build vocab
-        _.each(contents, function (content) {
-            _.each(content.split('\n'), function (line) {
-                var lineSplit = line.split('\t');
-                vocab.push({
-                    german: lineSplit[0],
-                    english: lineSplit[1],
-                    lexCat: lineSplit[2],
-                    extra: lineSplit[3]
+        buildSource = function (href) {
+            var pathToSource = vocabListPath + href,
+                $checkbox = $('<input />').attr('type', 'checkbox'),
+                $source = $('<div />')
+                    .append($checkbox)
+                    .append($('<a />').text(href).attr({
+                        'href': pathToSource,
+                        'target': '_blank'
+                    })),
+                data = {
+                    xhr: $.get(pathToSource)
+                };
+            data.xhr.done(function (resp) {
+                allVocab[href] = _.map(resp.split('\n'), function (line) {
+                    var lineSplit = line.split('\t');
+                    return {
+                        german: lineSplit[0],
+                        english: lineSplit[1],
+                        lexCat: lineSplit[2],
+                        extra: lineSplit[3]
+                    };
                 });
             });
-        });
+            $checkbox.on('click.flashcard', function (evt) {
+                if ($checkbox.is(':checked')) {
+                    availableVocab[href] = $source;
+                } else {
+                    delete availableVocab[href];
+                }
+            });
+            // Pre-select main vocab sections.
+            if (href.search(/grundwortschatz/i) !== -1) {
+                $checkbox.prop('checked', 'checked');
+                $checkbox.trigger('click.flashcard');
+                $checkbox.prop('checked', 'checked');
+            }
+            $source.data('flashcard', data);
+            return $source;
+        };
 
-        // Bind event handlers
-        $next.click(function (evt) {
-            var choice = vocab[Math.floor(Math.random() * vocab.length)];
-            $english.text(choice.english);
-            $german.hide().text(choice.german);
-            $lexCat.text(choice.lexCat);
-            $extra.text(choice.extra);
-        });
+    $indexLoader.load(vocabListPath, function () {
+        $.when.apply(this, _.map($('a', $indexLoader), function (el) {
+            var href = $(el).attr('href');
+            return buildSource(href).appendTo($sources).data('flashcard').xhr;
+        })).done(function () {
 
-        $reveal.click(function (evt) {
-            $german.show();
-        });
+            // Bind event handlers
+            $next.click(function (evt) {
+                $('div', $sources).each(function (i, div) {
+                    $(div).removeClass('sourced');
+                });
+                if (_.size(availableVocab) === 0) {
+                    $('#sad').fadeIn();
+                } else {
+                    $('#sad').fadeOut();
 
-        // Bootstrap
-        $next.click();
+                    var chosenSource = _.keys(availableVocab)[Math.floor(Math.random() * _.size(availableVocab))],
+                        vocabList = allVocab[chosenSource],
+                        choice = vocabList[Math.floor(Math.random() * vocabList.length)];
+                    availableVocab[chosenSource].addClass('sourced');
+                    $english.text(choice.english);
+                    $german.addClass('hidden').text(choice.german);
+                    $lexCat.text(choice.lexCat || '');
+                    $extra.text(choice.extra || '');
+                }
+            });
+
+            $reveal.click(function (evt) {
+                $german.removeClass('hidden');
+            });
+
+            // Bootstrap
+            $next.click();
+        });
     });
 });
